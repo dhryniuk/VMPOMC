58dfa1e7ac73d03a3c7686368df69d485cd85ad9
dhryniuk <dhryniuk@outlook.com>
2022-10-26 14:25:20 +0100

    reworked two level system

--- LVMC_exact.jl
+++ LVMC_exact.jl
@@ -4,19 +4,26 @@ include("ED_Lindblad.jl")
 using BenchmarkTools
 
 
-#L = make_Liouvillian(sp_H_field,sm)
-#L = make_Liouvillian(sx,sm)
-#display(L)
+J=1.0
+γ=1.0
 
-#vec=[0, 0, 1, 0]
+L = make_Liouvillian(J*sx,γ*sz)
 
-#display(transpose(vec))
+display(L)
+#error()
 
-#display(transpose(vec)*L)
-#display(transpose(vec)*L*vec)
+#trial_ρ = [1.0 1+0.0001im; 1-0.41im 0.2]
+#trial_ρ = [0.1 -0.2im; 0.2im 0.9]
+#trial_ρ = [0.5 -0.01im; 0.01im 0.5]
+#trial_ρ = [0.5 -0.9im; 1+0.1im 0.5]
+#trial_ρ = [4*J^2/(4*J^2+γ^2)+0.1 2im*J*γ/(4*J^2+γ^2)+0.1; -2im*J*γ/(4*J^2+γ^2)+0.1 1]
+#trial_ρ = [4/5+0.5 2im*J*γ/(4*J^2+γ^2)+0.5; -2im*J*γ/(4*J^2+γ^2) 1]
+trial_ρ=rand(ComplexF64, 2,2)
+#trial_ρ+=conj(trial_ρ)
+trial_ρ/=tr(trial_ρ)
 
-#r=[1,0]⊗[0,1]
-#display(r)
+init_trial_ρ = copy(trial_ρ)
+display(init_trial_ρ)
 
 basis=generate_bit_basis(1)
 display(basis)
@@ -66,15 +73,27 @@ function find_index(i)
     end
 end
 
+function calculate_Z(J,γ,trial_ρ)
+    Z = 0
+    for i in [1,0]
+        for j in [1,0]
+            local state = density_matrix(1,[i],[j])
+            αρ = trial_ρ[find_index(state.ket[1]) , find_index(state.bra[1])]
+            p_α = αρ*conj(αρ)
+            Z += p_α
+        end
+    end
+    return Z
+end
 
 function local_Lindbladian(sample::density_matrix,J,γ,ψ)
     local_L=0
 
     s = dVEC[(sample.ket[1],sample.bra[1])]
-    bar_L = transpose(s)*L
+    bra_L = transpose(s)*L
 
     for i in 1:4
-        loc = bar_L[i]
+        loc = bra_L[i]
         state = TPSC[i]
         local_L += loc*ψ[find_index(state[1]), find_index(state[2])]
     end
@@ -99,17 +118,6 @@ function calculate_mean_local_Lindbladian(J,γ,trial_ρ)
     return mean_local_Lindbladian/Z
 end
 
-J=0.0
-γ=1.0
-
-L = make_Liouvillian(J*sx,γ*sm)
-
-trial_ρ = [0.1 -0.2im; 0.2im 0.9]
-#trial_ρ = [0.000001 0.000001; 0.000001 1.0]
-
-display(calculate_mean_local_Lindbladian(J,γ,trial_ρ))
-
-error()
 
 function calculate_L∇L(J,γ,trial_ρ,ii,jj)
     Z=0
@@ -122,10 +130,14 @@ function calculate_L∇L(J,γ,trial_ρ,ii,jj)
             αρ = trial_ρ[find_index(sample.ket[1]) , find_index(sample.bra[1])]
             p_α = αρ*conj(αρ)
             Z+=p_α
+
+            s = dVEC[(sample.ket[1],sample.bra[1])]
+            bra_L = transpose(s)*L
+            
             for i in 1:4
-                loc = bar_L[i]
+                loc = bra_L[i]
                 state = TPSC[i]
-                local_L += loc*ψ[find_index(state[1]), find_index(state[2])]
+                local_L += loc*trial_ρ[find_index(state[1]), find_index(state[2])]
                 if ii==state[1] && jj==state[2]
                     ∇local_L += conj(loc)
                 end
@@ -137,134 +149,12 @@ function calculate_L∇L(J,γ,trial_ρ,ii,jj)
 end
 
 
-
-error()
-
-
-
-function dagger(state::density_matrix)
-    return density_matrix(conj(state.coeff),state.bra,state.ket)
-end
-
-
-function contributing_states(sample,J,γ)
-    terms = Dagger_Lindblad(sample,J,γ)
-
-    for i in 1:length(terms)
-        terms[i] = dagger(terms[i])
-    end
-
-    states = collect_states(terms)
-
-    return states
-end
-
-
-function local_Lindbladian(sample,J,γ,ψ)
-    local_L=0
-    contr_states = contributing_states(sample,J,γ)
-
-    for state in contr_states
-        local_L += ψ[find_index(state.ket[1]) , find_index(state.bra[1])]*state.coeff
-    end
-    local_L/=ψ[find_index(sample.ket[1]) , find_index(sample.bra[1])]
-    return local_L*conj(local_L)
-end
-
-function calculate_log_derivative(i,j,u,v,trial_ρ)
-    if i==u && j==v
-        return 1
-    else return 0
-    end
-end
-
-
-function calculate_L∇L(J,γ,trial_ρ, ii, jj)
-    Z=0
-    L∇L=0
-    for i in [1,-1]
-        for j in [1,-1]
-            local_L=0
-            ∇local_L=0
-            local sample = density_matrix(1,[i],[j])
-            αρ = trial_ρ[find_index(sample.ket[1]) , find_index(sample.bra[1])]
-            p_α = αρ*conj(αρ)
-            Z+=p_α
-            contr_states = contributing_states(sample,J,γ)
-            for state in contr_states
-                local_L += state.coeff*trial_ρ[find_index(state.ket[1]) , find_index(state.bra[1])]
-                ∇local_L += conj(state.coeff)*calculate_log_derivative(ii,jj,state.ket[1],state.bra[1],trial_ρ)
-            end
-            L∇L+=local_L*∇local_L
-        end
-    end
-    return L∇L/Z
-end
-
-# Need to iterate over states to calculate ⟨σ,η|ρ⟩
-# Then perform variational optimization
-
-
-#trial_ρ = [1.0 1+0.0001im; 1-0.41im 0.2]
-#trial_ρ = [0.1 -0.2im; 0.2im 0.9]
-#trial_ρ = [0.5 -0.9im; 1+0.1im 0.5]
-#trial_ρ = [4*J^2/(4*J^2+γ^2)+0.1 2im*J*γ/(4*J^2+γ^2)+0.1; -2im*J*γ/(4*J^2+γ^2)+0.1 1]
-#trial_ρ = [4/5+0.5 2im*J*γ/(4*J^2+γ^2)+0.5; -2im*J*γ/(4*J^2+γ^2) 1]
-trial_ρ=rand(ComplexF64, 2,2)
-#trial_ρ+=conj(trial_ρ)
-trial_ρ/=tr(trial_ρ)
-
-init_trial_ρ = copy(trial_ρ)
-
-#trial_ρ = [0.01 1.000; 1.000 0.01]
-
-display(tr(trial_ρ))
-#needs to be hermitian, unit trace, and positive
-
-display(trial_ρ)
-
-evals, evecs = eigen(trial_ρ)
-
-display(evals)
-
-function calculate_Z(J,γ,trial_ρ)
-    Z = 0
-    for i in [1,-1]
-        for j in [1,-1]
-            local state = density_matrix(1,[i],[j])
-            αρ = trial_ρ[find_index(state.ket[1]) , find_index(state.bra[1])]
-            p_α = αρ*conj(αρ)
-            Z += p_α
-        end
-    end
-    return Z
-end
-
-function calculate_mean_local_Lindbladian(J,γ,trial_ρ)
-    Z = calculate_Z(J,γ,trial_ρ)
-    mean_local_Lindbladian = 0
-    for i in [1,-1]
-        for j in [1,-1]
-            local state = density_matrix(1,[i],[j])
-            αρ = trial_ρ[find_index(state.ket[1]) , find_index(state.bra[1])]
-            p_α = αρ*conj(αρ)
-            L_local = local_Lindbladian(state,J,γ,trial_ρ)
-            mean_local_Lindbladian+=p_α*L_local
-        end
-    end
-    return mean_local_Lindbladian/Z
-end
-
-display(calculate_mean_local_Lindbladian(J,γ,trial_ρ))
-
-
-
 function gradient(J,γ,trial_ρ)
     Z = calculate_Z(J,γ,trial_ρ)
     mean_local_Lindbladian = calculate_mean_local_Lindbladian(J,γ,trial_ρ)
     F = zeros(ComplexF64, 2,2)
-    for i in [1,-1]
-        for j in [1,-1]
+    for i in [1,0]
+        for j in [1,0]
             local state = density_matrix(1,[i],[j])
             αρ = trial_ρ[find_index(state.ket[1]) , find_index(state.bra[1])]
             p_α = αρ*conj(αρ)
@@ -274,12 +164,14 @@ function gradient(J,γ,trial_ρ)
     return F./Z
 end
 
-F = gradient(J,γ,trial_ρ)
-display(F)
+
+#F = gradient(J,γ,trial_ρ)
+#display(F)
 #error()
-δχ = 0.1
+
+δχ = 0.01
 @time begin
-    for i in 1:10000
+    for i in 1:1000
         new_trial_ρ = trial_ρ - δχ*(gradient(J,γ,trial_ρ))
         global trial_ρ = new_trial_ρ
         global trial_ρ./=tr(trial_ρ)
